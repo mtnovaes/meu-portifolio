c80b62f717fdb55d2fc7d7313d8471ddcc660cc2
Matheus Novaes <fizzgou@gmail.com>
2025-12-03 18:53:22 -0300

    Upload inicial do portfólio

---
+++ app.js
@@ -0,0 +1,26 @@
+const techs = [
+    {
+        title: "HTML5",
+        desc: "Estrutura Semântica",
+        iconColor: "text-orange-500",
+        icon: `<svg class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" /></svg>`,
+
+    },
+    {
+        title: "Tailwind CSS",
+        desc: "Estilização Rápida",
+        iconColor: "text-cyan-400",
+        icon: `<svg class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" /></svg>`,
+
+    },
+    {
+        titulo: "JavaScript",
+        desc: "Interatividade",
+        iconColor: 'text-yellow-400',
+        icon:  `<svg class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 10V3L4 14h7v7l9-11h-7z" /></svg>`,
+    },
+    {
+        titulo: "Mobile First",
+        desc: "Responsividade"
+    }
+]
